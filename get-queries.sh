28b01d0323093b130c19660915f905f8a0a43676
noClaps <git@zerolimits.dev>
2025-04-20 09:52:45 +0100

    chore: Add injections.scm to get-queries.sh

--- get-queries.sh
+++ get-queries.sh
@@ -1,1 +1,2 @@
 curl -L 'https://github.com/tera-language/tree-sitter-teralang/raw/refs/heads/main/queries/highlights.scm' -o languages/teralang/highlights.scm
+curl -L 'https://github.com/tera-language/tree-sitter-teralang/raw/refs/heads/main/queries/injections.scm' -o languages/teralang/injections.scm
